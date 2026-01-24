001892a50ecbdd8f19e401e80fb7273206b32ef1
Heshan Tharusha <148676960+HeshanTDev@users.noreply.github.com>
2026-01-24 20:04:39 +0530

    Update endpoint url in status,department and type repos

--- src/main/java/com/heshant/usermanagement/repo/UserRoleRepository.java
+++ src/main/java/com/heshant/usermanagement/repo/UserRoleRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
-@RepositoryRestResource(path = "user-management/user_roles")
+@RepositoryRestResource(path = "user-management/user-roles")
 public interface UserRoleRepository extends JpaRepository<UserRole,Long> {
 }
